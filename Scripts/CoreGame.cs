b7465e1b30cfbe6295ad396771d5b1f1ef5949e6
isnizal <31788317+isnizal@users.noreply.github.com>
2020-04-06 20:20:54 +0800

    spawn defender at snap square

---
+++ Scripts/CoreGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreGame : MonoBehaviour
+{
+    [SerializeField] GameObject defenderPrefab;
+    // Start is called before the first frame update
+
+
+    private void OnMouseDown()
+    {
+        SpawnDefender(GetSquareClick());
+    }
+    Vector2 GetSquareClick()
+    {
+        Vector2 mousePos = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 snapToGrid = GetSnapClick(worldPos);
+        return snapToGrid;
+    }
+
+    Vector2 GetSnapClick(Vector2 worldPos)
+    {
+        float x = Mathf.RoundToInt(worldPos.x);
+        float y = Mathf.RoundToInt(worldPos.y);
+        return new Vector2(x,y);
+    }
+    void SpawnDefender(Vector2 defenderPos)
+    {
+        GameObject newDefender = Instantiate(defenderPrefab,defenderPos,Quaternion.identity) as GameObject;
+        Debug.Log(defenderPos);
+    }
+}
