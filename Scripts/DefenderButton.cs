aad95a40818fc25b3ac726c3afc56a18c5ccc30e
isnizal <31788317+isnizal@users.noreply.github.com>
2020-04-07 10:24:08 +0800

    Ui selection

---
+++ Scripts/DefenderButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    
+    SpriteRenderer my_Sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_Sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        var defenderButn = FindObjectsOfType<DefenderButton>();
+        foreach (DefenderButton defender in defenderButn)
+        {
+            defender.my_Sprite.color = new Color32(89, 89, 89, 255);
+        }
+        my_Sprite.color = Color.white;
+    }
+
+
+
+}
