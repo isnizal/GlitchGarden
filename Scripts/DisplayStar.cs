f22755c2d4a70ec435b69ddf741fb6c558925ca1
isnizal <31788317+isnizal@users.noreply.github.com>
2020-04-07 16:06:24 +0800

    displaying star

---
+++ Scripts/DisplayStar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayStar : MonoBehaviour
+{
+    [SerializeField] float star = 100f;
+    Text starText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        starText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void UpdateDisplay()
+    {
+        starText.text = star.ToString();
+    }
+
+    void AddStar(int amount)
+    {
+        star += amount;
+        UpdateDisplay();
+    }
+
+    void SpendStar(int amount)
+    {
+        if (star >= amount)
+        {
+            star -= amount;
+            UpdateDisplay();
+        }
+    }
+}
