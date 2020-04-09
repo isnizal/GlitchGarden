bbe8f3a759cb6c8adaa24223f9623c30452424a8
isnizal <31788317+isnizal@users.noreply.github.com>
2020-04-09 12:13:52 +0800

    Preparing defender to shoot
    if defender same position with attacker will shoot

--- Scripts/Defender.cs
+++ Scripts/Defender.cs
@@ -7,19 +7,31 @@ public class Defender : MonoBehaviour
     [Header("Stats")]
     [SerializeField] float defenderHealth = 10f;
     [SerializeField] float projectileSpeed;
+    [Header("AttackerSpawn")]
+    AttackerSpawn attackerSpawner;
+
     [SerializeField] GameObject projectile;
     [SerializeField] Transform gunLoad;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetLaneAttacker();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isAttackerInLane())
+        {
+            
+            Debug.Log("shoot pew pew");
+        }
+        else
+        {
+            Debug.Log("ready to fire");
+        }
     }
+
 
     public void Shoot()
     {
@@ -34,5 +46,31 @@ public class Defender : MonoBehaviour
         defenderHealth = attackerHealth;
         Debug.Log("Hello");
         
+    }
+    private void SetLaneAttacker()
+    {
+        AttackerSpawn[] attackerSpawners = FindObjectsOfType<AttackerSpawn>();
+
+        foreach (AttackerSpawn spawner in attackerSpawners)
+        {
+            bool isCloseEnough = Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon;
+            if (isCloseEnough)
+            {
+                attackerSpawner = spawner;
+            }
+        }
+    }
+
+    private bool isAttackerInLane()
+    {
+        if (attackerSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
     }
 }
