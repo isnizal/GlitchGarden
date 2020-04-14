9b3188274b1d18577079d2a4697130c6b964861c
isnizal <31788317+isnizal@users.noreply.github.com>
2020-04-14 17:40:43 +0800

    wall health

---
+++ Scripts/WallHealthDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WallHealthDisplay : Defender
+{
+    float damage = 20f;
+
+    private void Start()
+    {
+        baseWallText.text = wallhitpoints.ToString();
+    }
+    private void Update()
+    {
+        UpdateBaseWallText();
+        if (wallhitpoints <= 0)
+        {
+            SceneManager.LoadScene("LoseLevel");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        wallHealth(damage);
+    }
+}
