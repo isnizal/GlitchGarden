7514f13e84d680712d96d5081d09834cd426ef15
isnizal <31788317+isnizal@users.noreply.github.com>
2020-05-05 18:07:56 +0800

    showing up health bar

--- Scripts/Attacker.cs
+++ Scripts/Attacker.cs
@@ -1,19 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Attacker : MonoBehaviour
 {
     [Header("Stats")]
     [SerializeField] float health = 10;
+    [SerializeField] float damage = 10;
     [SerializeField] GameObject deadParticleVFX;
+    [SerializeField] GameObject HealthBar;
+    [SerializeField] GameObject HealthBarPosition;
+    [SerializeField] GameObject attackerSlider;
+    float attackerValue;
     [Range(0f, 4f)] [SerializeField] float currentSpeed = 1f;
     Animator animator;
     GameObject currentTarget;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        FindObjectOfType<LevelController>().AttackerSpawn();
+    }
+
+    private void OnDestroy()
+    {
+        LevelController levelController = FindObjectOfType<LevelController>();
+        if (levelController == null)
+        {
+            return;
+        }
+        else
+        {
+            FindObjectOfType<LevelController>().AttackerKilled();
+        }
+        
+    }
     void Start()
     {
-        animator = GetComponent<Animator>();
+
+        InstantiateHealthBar();
+        
+        
     }
 
     // Update is called once per frame
@@ -21,8 +49,46 @@ public class Attacker : MonoBehaviour
     {
         HealthStatus();
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
+        updateHealthBar(HealthBar);
+        //put instantiatehealth bar on update
+        
+        
+        
+    }
+    void updateHealthBar(GameObject healthBar)
+    {
+        Slider slide = healthBar.GetComponentInChildren<Slider>();
+        HealthValue(slide);
     }
 
+    void InstantiateHealthBar()
+    {
+        GameObject healthBar = Instantiate(HealthBar, HealthBarPosition.transform.position, transform.rotation) as GameObject;
+        //if healthbar more than 1 destroy it
+        healthBar.transform.parent = transform;
+        updateHealthBar(healthBar);
+
+    }
+    void HealthValue(Slider slide)
+    {
+        attackerValue = health / health;
+        slide.value = attackerValue;
+        Debug.Log(attackerValue);
+        
+    }
+
+    void UpdateAnimationState()
+    {
+        if (!currentTarget)
+        {
+           
+            GetComponent<Animator>().SetBool("isAttacking", false);
+            GetComponent<Animator>().SetBool("isJump", false);
+
+        }
+
+    }
     public float SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
@@ -31,7 +97,13 @@ public class Attacker : MonoBehaviour
     public float GetHealth(float damage)
     {
         health -= damage;
+        
         return health;
+    }
+
+    public float GetDamage()
+    {
+        return damage;
     }
 
     public void HealthStatus()
@@ -42,6 +114,7 @@ public class Attacker : MonoBehaviour
             GameObject deadParticle = Instantiate(deadParticleVFX, transform.position, transform.rotation)
                 as GameObject;
             Destroy(deadParticle, 3f);
+            FindObjectOfType<LevelController>().AttackerKilled();
         }
 
         return;
@@ -49,8 +122,32 @@ public class Attacker : MonoBehaviour
 
     public void Attack(GameObject target)
     {
-        animator.SetBool("isAttacking", true);
+        GetComponent<Animator>().SetBool("isAttacking", true);
         currentTarget = target;
+        
     }
+    public void Jump(GameObject target)
+    {
+        GetComponent<Animator>().SetTrigger("isJump");
+        currentTarget = target;
+
+    }
+ 
+
+    public void AttackCurrentTarget(float damage)
+    {
+        if (!currentTarget)
+            return;
+       Defender health = currentTarget.GetComponent<Defender>();
+
+        if (health)
+        {
+            health.Health(damage);
+           
+        }
+
+    }
+
+
 
 }
